aeca37357b4391aaccbf4c4a8499c27ad6c409e0
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-01-17 19:08:15 +0100

    testing api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p v-if="response.permissions"><HelloWorld msg='Welcome to Your Vue.js App'/> {{response.name}}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data:function()
+  {
+    return {
+      Id: 3,
+      response: {
+        name: "",
+        surname1: "",
+        surname2: "",
+        dni: "",
+        tipo: "",
+        id: undefined,
+        permissions: [],
+        borrado: 0,
+      },
+    }
+  },
+  mounted:function()
+  {
+    axios.get("http://90.68.35.159:8081/newMenu.php?funcion=getEmployeeById&id="+ this.Id)
+    .then( data => {
+      this.response = data.data;
+      console.log(this.response.name);
+    });
   }
 }
 </script>
