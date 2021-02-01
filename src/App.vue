dfa27d7f43c4f98b07b8e37fac52a044972d7c9a
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-02-01 22:01:34 +0100

    update progress

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<div v-if="page == 'Login'" id="Login">
   <!-- Login -->
   <div class="body">
 		<div class="cabecera">
@@ -22,6 +23,29 @@
 			<p>Trabajo realizado por Jose Javier Valero Fuentes y Juan Francisco Navarro Ramiro para el curso de ASIR 2º X</p>
 		</div>
   </div>
+</div>
+<!-- Menu -->
+<div v-if="page == 'Menu'" id="Menu">
+  <div class="cabecera">
+    <p class="mensaje">Bienvenido {{user.name}} {{user.surname1}} {{user.surname2}}</p>
+    <div class="Logo">
+      <a href="veremp.php?funcion=Logout">
+        <img class="cierra" src="./shutdown.png" alt="Cerrar sesión" />
+      </a>
+    </div>
+    <div class="opciones">
+      <a v-if="user.permissions.includes('13')" class="link" href="veremp.php?funcion=Agregar_parte&id_emp={{user.id}}&dni={{user.dni}}">Crear parte</a>
+      <a v-if="incidencesCount >0" class="link" href="veremp.php?id_emp={{user.id}}&dni={{user.dni}}&funcion=Partes">Ver partes</a>
+      <a v-if="user.permissions.includes('2')" class="link" href="veremp.php?funcion=Estadisticas&id_emp={{user.id}}&dni={{user.dni}}">Estadísticas</a> 
+      <a v-if="user.permissions.includes('16')" class="link" href="veremp.php?funcion=Lista&id_emp={{user.id}}&dni={{user.dni}}">Lista empleados</a> 
+      <a class="link" href="veremp.php?funcion=Datos_personales&id_emp={{user.id}}&dni={{user.dni}}">Datos personales</a>
+    </div>
+  </div>
+    <div class="cuerpo">'.$_SESSION['mensaje'].'</div>
+    <div class="Pie">
+      <p>Trabajo realizado por Jose Javier Valero Fuentes y Juan Francisco Navarro Ramiro para el curso de ASIR 2º X</p>
+    </div>
+</div>
 </template>
 
 <script>
@@ -34,29 +58,19 @@ export default {
   data:function()
   {
     return {
-      Id: 3,
-      response: {
-        name: "",
-        surname1: "",
-        surname2: "",
-        dni: "",
-        tipo: "",
-        id: undefined,
-        permissions: [],
-        borrado: 0,
-      },
+      page: 'Login',
+      module: 'Main',
       form: {
         username: undefined,
         pass: undefined,
       },
       user: undefined,
       message: "",
+      incidences: undefined,
+      incidencesCount: 0,
     }
   },
   methods: {
-    messageUser: function() {
-      return "Welcome to Your Vue.js App " + this.response.name + " " + this.response.surname1;
-    },
     onSubmit: function()
     {
       axios.get("http://localhost:8082/newMenu.php?funcion=checkCredentials&username="+ this.form.username+"&pass="+this.form.pass)
@@ -65,10 +79,45 @@ export default {
         axios.get("http://localhost:8082/newMenu.php?funcion=getEmployeeByUsername&username="+ this.form.username)
         .then( data => {
           this.user = data.data;
+          this.page = 'Menu';
+          axios.get("http://localhost:8082/newMenu.php?funcion=getAllincidences")
+          .then( data => {
+            this.incidences = data.data;
+              axios.get("http://localhost:8082/newMenu.php?funcion=getAllincidences")
+              .then( data => {
+                this.incidences = data.data;
+                this.showIncidences();
+              });
+          });
         });
         console.log(this.response.username);
       });
-    }
+    },
+    showIncidences:function()
+    {
+      let new_array = undefined;
+      if(this.user.permissions.includes("7") && this.user.permissions.includes("8") && this.user.permissions.includes("9"))
+      {
+          new_array = this.incidences.filter(array => {
+              return (array.owner.id == this.user.id);
+          });
+          this.incidencesCount = new_array.length;
+      }
+      else if (this.user.permissions.includes("3") && this.user.permissions.includes("4") && this.user.permissions.includes("5"))
+      {
+          new_array = this.incidences.filter(array => {
+              return (array.solver.id == this.user.id || array.state == 1);
+          });
+          this.incidencesCount = new_array.length;
+      }
+      else if (this.user.permissions.includes("6") && this.user.permissions.includes("7") && this.user.permissions.includes("8") && this.user.permissions.includes("9") && this.user.permissions.includes("10") && this.user.permissions.includes("11") && this.user.permissions.includes("12")) 
+      {
+          new_array = this.incidences.filter(array => {
+              return (array.solver.id == this.user.id || (array.state == 1 || array.state == 2 || array.state == 3 || array.state == 4) || array.owner.id == this.user.id);
+          });
+          this.incidencesCount = new_array.length;
+      }
+    },
   },
   mounted:function()
   {
