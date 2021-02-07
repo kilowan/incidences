b3560ad256f5b9edd65381b9353b44f431c6a849
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-02-07 23:32:59 +0100

    update progress

--- src/components/userPanel.vue
+++ src/components/userPanel.vue
@@ -1,17 +1,21 @@
 <template>
   <!-- userInfo -->
   <user-info :user="user"/>
+  <!-- userInfo -->
+  <statistics :user="user"/>
 </template>
 
 <script>
 
 import userInfo from './userInfo.vue';
+import statistics from './statistics.vue';
 
 export default {
   name: 'userPanel',
   props: ['user'],
   components: {
     userInfo,
+    statistics,
   },
   data:function()
   {
